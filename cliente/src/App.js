cf2cbb809124a4690213abade2fc64c3205721f1
Kleryston <Kleryston@jc.com.br>
2020-01-11 03:45:45 -0300

    Página Search

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
-      Kleryston Thiago
+      <Search/>
     </div>
   );
 }
---
+++ cliente/src/pages/Search.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import '../assets/css/search.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon className="ico-search" icon={faSearch} />
+
+export default class Search extends Component {
+    render() {
+        return (
+            <div>
+                <div className="myContainer d-flex align-items-center align-self-center">
+                    <div className="Home d-flex align-items-center justify-content-center">
+                        <div className="row justify-content-center">
+                            <div className="textSearch"><h1 className="Github-Search">GitHub <span className="text-style-1">Search</span></h1></div>
+                            <div className="input-group justify-content-center">
+                                <input type="text" className="Search-Input" placeholder="" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                                <div className="input-group-append">
+                                    <button className="btn btn-outline-secondary button" type="button" id="button-addon2">                                        
+                                        {element}
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
